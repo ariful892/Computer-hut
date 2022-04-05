9f67333ddecd884700e563a44c3fe733e8ea8b22
ariful892 <ariful00892@gmail.com>
2022-04-05 23:41:36 +0600

    blogs route added

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -6,29 +6,17 @@ const Blogs = () => {
     return (
         <div className='blogs'>
             <h2 className='review-title'>HTML Concepts</h2>
-            <Accordion defaultActiveKey="0" flush>
+            <Accordion defaultActiveKey="0" flush className='blog'>
                 <Accordion.Item eventKey="0">
                     <Accordion.Header>What is Semantic Tag?</Accordion.Header>
                     <Accordion.Body>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-                        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-                        est laborum.
+                        Semantic tag in html5 is used to make the website more meaningful. It makes the code easier to understand by those tags. By seeing the tag one can easily understand what part fo the website it will be. Suppose the header tag is used in the upper side of the website and normally the website title and nav bar is kept in this tag. Then in the main all the section of the website is kept. In section tag many article tag can be used. Also there are audio and video tag are available in html5. Overall semantic are are very useful to organize the website beautifully.
                     </Accordion.Body>
                 </Accordion.Item>
                 <Accordion.Item eventKey="1">
                     <Accordion.Header>Difference between inline, block and inline-block?</Accordion.Header>
                     <Accordion.Body>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-                        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-                        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
-                        est laborum.
+                        The main main difference between inline,inline-block and block element is height - width. In inline element it takes the fixed height and width. We can not set height and width in inline element .The inline elements stay beside one another they don't create a new line. The inline-block element is like inline element but it act like block element. They also go beside one another but we can set height width in them. The block element takes the full width of the line and one specific element take one single line . We can set height width margin padding everything in block element.
                     </Accordion.Body>
                 </Accordion.Item>
             </Accordion>
